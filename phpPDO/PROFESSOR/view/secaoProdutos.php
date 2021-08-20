f18b967aaa861ca8f2d24b2f421c512cd45174e2
HaristonNuMacedo <haristonmacedo2001@gmail.com>
2021-08-20 11:41:27 -0300

    Depois :microbe:

---
+++ phpPDO/PROFESSOR/view/secaoProdutos.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Secao de Produtos</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+
+    <style>
+        .season2{
+            display: inline-block;
+            padding-top: 10px;
+            padding-bottom: 0px;
+            text-align: center;
+            width: 100%;
+            background-color: #212529;
+            border-top: 5px solid rgb(206, 206, 206);
+            border-left: 85px solid rgb(206, 206, 206);
+            border-right: 85px solid rgb(206, 206, 206);
+            color:white;
+            font-size: 30px;
+        }
+    </style>
+    
+</head>
+<body style="background-color: rgb(206, 206, 206);">
+       <!-- MENU DO SITE -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+
+          <a class="navbar-brand" href="#" style="font-size: 30px;"><img src="img/amd-white-logo-1260x709.png" 
+                style="width: 130px; height: auto; margin-left: 10px; margin-top: -8px;"></a>
+
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+              <ul class="navbar-nav" style="margin-left: 10px;">
+                <button type="button" class="btn btn-secondary" style="background-color: transparent; margin-left: 10px;">
+                  <a href="#processor" style="text-decoration: none; color: white;">Processamento Engine</a></button>
+
+                <button type="button" class="btn btn-secondary" style="background-color: transparent; margin-left: 10px;">
+                  <a href="#graphics" style="text-decoration: none; color: white;">Diretório Gráfico</a></button>
+
+                <button type="button" class="btn btn-secondary" style="background-color: transparent; margin-left: 10px;">
+                  <a href="#servicos" style="text-decoration: none; color: white;">Nossos Serviços</a></button>
+
+                <div class="LoginForm">
+                  
+                  <button type="button" class="btn btn-secondary" style="background-color: transparent; margin-left: 10px;">
+                    <a href="CadastroAcademy.html" style="text-decoration: none; color: white;">CADASTRE</a></button>
+
+                  <button type="button" class="btn btn-secondary" style="background-color: transparent; margin-left: 10px;">
+                    <a href="LoginAcadamy.html" style="text-decoration: none; color: white;">LOGIN</a></button>
+
+                  <label>No Login</label>
+                </div>
+              </ul>
+          </div>
+        </div>
+      </nav> 
+
+        <section class="season2">
+            <p>VEJA NOSSOS PRODUTOS</p>
+        </section>
+
+      <div class="container">
+
+            <div class="row" style="background-color: rgb(206, 206, 206); margin-top: 50px;">
+                <div class="col-lg-3">
+                    <div class="col-10 offset-2">
+                        <img class="bd-placeholder-img" width="200" height="200" src="img/dumbbel.png" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+                    </div>
+
+                    <h4 style="text-align: center;">Produto</h4>
+                    <p>Descrição do produto posto na seção.</p>
+                    <!--<p><a class="btn btn-secondary" href="#">SAIBA MAIS &raquo;</a></p>-->
+                </div>
+                <div class="col-lg-3">
+                    <div class="col-10 offset-2">
+                        <img class="bd-placeholder-img" width="200" height="200" src="img/dumbbel.png" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+                    </div>
+
+                    <h4 style="text-align: center;">Produto</h4>
+                    <p>Descrição do produto posto na seção.</p>
+                    <!--<p><a class="btn btn-secondary" href="#">SAIBA MAIS &raquo;</a></p>-->
+                </div>
+                <div class="col-lg-3">
+                    <div class="col-10 offset-2">
+                        <img class="bd-placeholder-img" width="200" height="200" src="img/weight-lifting-levantamento.png" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+                    </div>
+
+                    <h4 style="text-align: center;">Produto</h4>
+                    <p>Descrição do produto posto na seção.</p>
+                    <!--<p><a class="btn btn-secondary" href="#">SAIBA MAIS &raquo;</a></p>-->
+                </div>
+                <div class="col-lg-3">
+                    <div class="col-10 offset-2">
+                        <img class="bd-placeholder-img" width="200" height="200" src="img/weight-plates.png" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+                    </div>
+
+                    <h4 style="text-align: center;">Produto</h4>
+                    <p>Descrição do produto posto na seção.</p>
+                    <!--<p><a class="btn btn-secondary" href="#">SAIBA MAIS &raquo;</a></p>-->
+                </div>
+            </div>       
+        </div>
+
+</body>
+</html>
